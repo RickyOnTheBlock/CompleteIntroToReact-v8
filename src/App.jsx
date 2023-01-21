340b5934934f37eb130be2fafb409006af4e8583
Ricky On The Block <rickyontheblock@pm.me>
2023-01-20 22:16:56 -0700

    Vite setup and config

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 // props => properties!
 const Pet = (props) => {
   return React.createElement("div", {}, [
@@ -29,5 +32,5 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
