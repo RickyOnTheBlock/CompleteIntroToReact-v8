faa153d6ba579a50b31d360db61eb26f5c735939
Ricky On The Block <rickyontheblock@pm.me>
2023-01-21 13:30:50 -0700

    Hooks: Array Mapping to React Components (HTML)

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
+const ANIMALS = ["birds", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   // State hooks allow react to work with mutable state
   const [location, setLocation] = useState("Phoenix, AX");
+  const [animal, setAnimal] = useState();
+  const [breed, setBreed] = useState("");
+
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -15,6 +20,38 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {/* Turn the ANIMALS array into an array of React Components (HTML) */}
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            disabled={breeds.length === 0}
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
